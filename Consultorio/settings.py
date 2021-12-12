007ae0bbc37744f207533873be1af34458d29e65
Javier <31666461+AlekseiRovenich@users.noreply.github.com>
2021-12-11 20:59:16 -0600

    Update settings.py

--- Consultorio/settings.py
+++ Consultorio/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!n3ckt#3qt_9+_y9%jj@sxw5p$9cv^bt@psxhtyj(mdufp^hk0'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
